6c85ff061da943e61069a7fdc61265cbabb9332b
Xurshidjohn <xurshidjonxurshid45@gmail.com>
2025-06-21 14:48:08 +0500

    first commit

---
+++ src/index.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char *soz = "install=`gcc main.c -o bin/app`";
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
