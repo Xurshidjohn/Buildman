b058c370ca0eb2dce70d5fbd9fe22c9719d9d5ca
Xurshidjohn <xurshidjonxurshid45@gmail.com>
2025-06-21 23:45:50 +0500

    install, clear, zero, xurshid

--- src/index.c
+++ src/index.c
@@ -2,39 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char *soz = "[install] = `gcc main.c -o bin/app`\n[clear] = `rm bin/app`";
+const char *soz = "";
 
 struct Line {
     char key[100];
     char command[300];
 };
 
-struct Line lines[100];
 
-int main(int argc, char const *argv[])
-{
+void get_config_keys() {
+    char *configCodes = malloc(sizeof(char) * 1000);
+
+    FILE * file = fopen("build.config", "r");
     int i =0;
     int key_i = 0;
     char key_val[100];
     int temp = 0;
-    while(soz[i] != '\0') {
-        if(soz[i] == '[') {
+    char ch;
+    while((ch = fgetc(file)) != EOF) {
+        if(ch == '[') {
             temp = 1;
+            key_val[0] = '\0';
             key_i = 0;
             i++;
             continue;
         }
 
-        if(soz[i] == ']') {
+        if(ch == ']') {
             key_val[key_i] = '\0';
+            printf("%s\n", key_val);
+            key_val[0] = '\0';
             temp = 0;
             i++;
             continue;
         }
         if (temp == 1) {
-            key_val[key_i++] = soz[i];
+            key_val[key_i++] = ch;
         }
         i++;
     }
+}
+
+struct Line lines[100];
+
+int main(int argc, char const *argv[])
+{  
+    get_config_keys();
     return 0;
 }
