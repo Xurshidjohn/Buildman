95ad3060fa275c3d37a37762ecec4d2e79b9194e
Xurshidjohn <xurshidjonxurshid45@gmail.com>
2025-06-22 13:43:20 +0500

    lake standard ravishda ishlamoqda, endi lakeni windowsda ham linuxda ham o'rnatish mumkin

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
-if [[ "$OSTYPE" == "linux-gnu"]]; then
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	gcc src/index.c -o lake
 	mv lake /usr/local/bin
+fi
+
+if [[ "$OSTYPE" == "msys" ]]; then
+	gcc src/index.c -o lake
+	setx PATH "C:/Users/$USERNAME/Downloads/lake"
+fi
